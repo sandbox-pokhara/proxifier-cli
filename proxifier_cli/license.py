4f9cdd8539230cc8baa3e146b48f9370bc879c1d
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2023-07-11 22:43:44 +0545

    fix!: key variable name conflict

--- proxifier_cli/license.py
+++ proxifier_cli/license.py
@@ -1,11 +1,9 @@
 import winreg as wrg
 
 
-def install_license(key, owner):
-    location = wrg.HKEY_CURRENT_USER
-    soft = wrg.OpenKeyEx(location, r"SOFTWARE\\")
-    key = wrg.CreateKey(soft, "Initex\\Proxifier\\License")
-    wrg.SetValueEx(key, "Key", 0, wrg.REG_SZ, key)
+def install_license(license_key, owner):
+    key = wrg.CreateKey(wrg.HKEY_CURRENT_USER, r"SOFTWARE\\Initex\\Proxifier\\License")
+    wrg.SetValueEx(key, "Key", 0, wrg.REG_SZ, license_key)
     wrg.SetValueEx(key, "Owner", 0, wrg.REG_SZ, owner)
     if key:
         wrg.CloseKey(key)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="proxifier-cli",
-    version="1.0.2",
+    version="1.0.3",
     author="Pradish Bijukchhe",
     author_email="pradishbijukchhe@gmail.com",
     description="CLI to set up proxy configuration of Proxifier",

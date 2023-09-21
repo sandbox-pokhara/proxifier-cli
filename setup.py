d7701056f24aad721dcfb044b85861379ddd0f0b
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2023-09-22 00:57:03 +0545

    chore: release 1.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="proxifier-cli",
-    version="1.0.4",
+    version="1.0.5",
     author="Pradish Bijukchhe",
     author_email="pradishbijukchhe@gmail.com",
     description="CLI to set up proxy configuration of Proxifier",

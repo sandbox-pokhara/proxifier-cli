bf910e872b2abcd269ccea4feca2cb6cd1183f98
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2023-09-22 00:48:14 +0545

    docs: add readme

---
+++ README.md
@@ -0,0 +1,19 @@
+# proxifier-cli
+
+CLI to set up proxy configuration of Proxifier
+
+## Installation
+
+You can install the package via pip:
+
+```bash
+pip install proxifier-cli
+```
+
+## License
+
+This project is licensed under the terms of the MIT license.
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 import setuptools
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="proxifier-cli",
     version="1.0.4",
     author="Pradish Bijukchhe",
     author_email="pradishbijukchhe@gmail.com",
     description="CLI to set up proxy configuration of Proxifier",
+    long_description=long_description,
     url="https://github.com/sandbox-pokhara/proxifier-cli",
     project_urls={
         "Bug Tracker": "https://github.com/sandbox-pokhara/proxifier-cli/issues",
